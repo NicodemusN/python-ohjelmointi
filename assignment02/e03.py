62e45de6096ebd273875e31ac154c11e8ca62556
NicodemusN <87670335+NicodemusN@users.noreply.github.com>
2025-09-11 15:49:27 +0300

    Koodaustehtävät lista 2

---
+++ assignment02/e03.py
@@ -0,0 +1,8 @@
+print("Anna kellonaika tunneissa")
+aika = int(input())
+#
+#if aika >= 9 and aika <= 17:
+#    print("Työaika käynnissä")
+
+if aika in range(9, 18):
+    print("Työaika on käynnissä")
---
+++ assignment02/e10.py
@@ -0,0 +1,5 @@
+print("Anna leveys")
+leveys = int(input())
+
+for x in range(0, leveys):
+    print("X", end="")
