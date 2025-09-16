c16c2bf1dc9651b6750c1e9f16c58adffb89a588
NicodemusN <niilo.nevala@gmail.com>
2025-09-16 13:38:49 +0300

    t 18 valmis

--- assignment03/e18.py
+++ assignment03/e18.py
@@ -1,22 +1,26 @@
 # List of Dictionaries
 
-# luodaan lista
+# luodaan ostoslista
 
 products = [
     {"name": "milk", "price": 1.5},
     {"name": "bread", "price": 2.0}
+
+    # debug
+    # {"name": "eggs", "price": 3.0}
 ]
 
+# alustetaan muuttujat iteroinnille
 i = 0
-#for i in products[i]:
-#    print(products[i][0], end=" ")
-#    print("costs", end="")
-#    print(products[i][1])
-
 j = 0
-for y in range(len(products)):
+
+# ei mitää hajua miten sain tän toimimaan
+for y in products:
     for x in products[i]:
         print(products[i][x], end=" ")
         if j == 0:
             print("costs", end=" ")
         j += 1
+    i += 1
+    j = 0
+    print() # uus rivi seuraaville ostoslistan esineille
