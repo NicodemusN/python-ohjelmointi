783f1aa071ad217c61fe6c858601c70b60b42fe2
NicodemusN <niilo.nevala@gmail.com>
2025-09-16 23:05:02 +0300

    Cleanup ja 19 aloitus

--- assignment03/e19.py
+++ assignment03/e19.py
@@ -1,5 +1,13 @@
 # Count Occurrences
 
-a = input("Anna sanoja muodossa sana1,sana2,sana3 ja niin edelleen")
-a = s.split(",")
+a = input("Anna sanoja muodossa sana1,sana2,sana3 ja niin edelleen: ")
+lst = a.split(",")
 
+dct = {}
+
+for i in range(len(lst)):
+    x = lst[i]
+    dct[x] = 0
+
+
+print(dct)
