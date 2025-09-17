52302ae655393743bb4af968a8ed8484b17505d9
NicodemusN <niilo.nevala@gmail.com>
2025-09-17 15:29:28 +0300

    aloitettu e20

---
+++ assignment03/e20.py
@@ -0,0 +1,13 @@
+# Nested Dictionary
+
+# alustetaan dictionary
+
+students = {
+    "Alice": {"age": 21, "major": "CS"},
+    "Bob": {"age": 22, "major": "Math"}
+}
+run = 0
+while run < 2:
+    print("[1] Add new student")
+    print("[2] Exit")
+    run = int(input())
