fe46e4bdd6c27194ae38797c626474d7c689046d
NicodemusN <niilo.nevala@gmail.com>
2025-09-17 10:43:31 +0300

    e019 vihdoin valmis

--- assignment03/e19.py
+++ assignment03/e19.py
@@ -1,13 +1,23 @@
 # Count Occurrences
 
+# otetaan käyttäjän syöte
 a = input("Anna sanoja muodossa sana1,sana2,sana3 ja niin edelleen: ")
 lst = a.split(",")
 
+# alustetaan dictionary
 dct = {}
 
-for i in range(len(lst)):
-    x = lst[i]
-    dct[x] = 0
+# loopataan jokainen listan jäsen
+for i in lst:
 
+    # setdefault() on toiminto, jolla voidaan lisätä
+    # arvoja dictionaryyn riippuen siitä, onko "avain"
+    # jo olemassa dictionaryssa
+
+    dct.setdefault(i, 0) # listan ensimmäinen jäsen saa arvon 0
+    dct[i] += 1 # arvoa lisätään yhdellä, jos jäsen tulee uudelleen
+                # vastaan toisella loopin kierroksella, arvo kasvaa
+                # jälleen yhdellä.
+                # näin saadaan laskettua jokaisen syötetyn sanan määrä
 
 print(dct)
