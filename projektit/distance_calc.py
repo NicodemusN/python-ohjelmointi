7c4266906c8a1e8468ddff5b23ec1747c2c04d5e
NicodemusN <niilo.nevala@gmail.com>
2025-09-18 15:34:03 +0300

    Etäisyyslaskimen alustus

---
+++ projektit/distance_calc.py
@@ -0,0 +1,9 @@
+# Laskin kahden maapallon pisteen väliselle suorimmalle matkalle maan pintaa pitkin
+# Ohjelmaan syötetään pisteiden koordinaatit asteiden tarkkuudella
+
+import math
+
+print("Arvot x-akselilla ovat pituusasteita, arvot y-akselilla ovat leveysasteita.")
+k1 = int(input("Anna ensimmäisen pisteen koordinaatit muodossa x,y: "))
+k2 = int(input("Anna toisen pisteen koordinaatit muodossa x,y: "))
+
