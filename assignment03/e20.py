9fd60bf10391b7dd55f3913feb9a3c579187efc8
NicodemusN <niilo.nevala@gmail.com>
2025-09-23 09:58:53 +0300

    jatkoa e20

--- assignment03/e20.py
+++ assignment03/e20.py
@@ -6,8 +6,23 @@ students = {
     "Alice": {"age": 21, "major": "CS"},
     "Bob": {"age": 22, "major": "Math"}
 }
+
+lst = list()
+
+# "main loop"
 run = 0
 while run < 2:
-    print("[1] Add new student")
+    print("[1] Lisää uusi opiskelija")
     print("[2] Exit")
     run = int(input())
+    
+    if run == 1:
+        name = input("Anna nimi: ")
+        age = input("Anna ikä: ")
+        major = input("Anna ala: ")
+
+        lst.insert(0, name)
+        lst.insert(1, age)
+        lst.insert(2, major)
+
+print(lst)
