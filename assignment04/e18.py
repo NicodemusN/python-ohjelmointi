4de1d0118a317a83b85832eac76bf61926825b5b
NicodemusN <niilo.nevala@gmail.com>
2025-09-24 13:41:40 +0300

    sekalaista

---
+++ assignment04/e18.py
@@ -0,0 +1,14 @@
+def tulosta(merkki, maara):
+    for _ in range(maara):
+        print(merkki, end="")
+
+korkeus = int(input("anna korkeus"))
+
+for rivi in range(korkeus):
+    if rivi == 0 or rivi == korkeus -1:
+        tulosta("X", korkeus)
+    else:
+        print("X", end="")
+        tulosta(" ", korkeus - 2)
+        print("X", end="")
+    print()§
---
+++ cplusplus/distanceCalc.cpp
@@ -0,0 +1,23 @@
+// Laskin kahden maapallon pisteen väliselle suorimmalle matkalle maan pintaa pitkin
+// Ohjelmaan syötetään pisteiden koordinaatit asteiden desimaalien tarkkuudella
+
+// Include the cmath library
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    double k1;
+    double k2;
+    int radius = 6371;
+    int theta = 0;
+
+    cout << "Anna ensimmäisen pisteen koordinaatit y,x:";
+    cin >> k1;
+    cout << "Anna toisen pisteen koordinaatit y,x:";
+    cin >> k2;
+
+
+
+    return 0;
+}
